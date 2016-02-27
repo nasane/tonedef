bf609f21d4f7a58e63a173d4344f6b7e370281df
Nathan Bossart <nbossart@live.com>
2016-02-27 15:31:41 -0800

    First attempt at test coverage.

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 tonedef: tonedef.c
-	cc -fPIC -std=c99 --shared -o libtonedef.so tonedef.c
+	cc -fPIC -std=c99 --shared -o libtonedef.so tonedef.c -fprofile-arcs -ftest-coverage
+tests: test.c
+	cc -std=c99 -o test test.c libtonedef.so
 clean:
-	rm -rf libtonedef.so
+	rm -rf libtonedef.so test
--- test.c
+++ test.c
@@ -7,18 +7,33 @@ int main(int argc, const char *argv[]) {
 	struct note exact_note;
 	struct note approx_note;
 
-	exact_note = get_exact_note(strtod(argv[1], NULL));
-	printf("%s %d %+f cents\n",
-		get_semitone_str(exact_note.semitone),
-		exact_note.octave,
-		exact_note.cents);
+	exact_note = get_exact_note(440.0);
+	if (exact_note.semitone != A || exact_note.octave != 4 || exact_note.cents != 0.0) {
+		fprintf(
+			stderr,
+			"expected: %s %d %f, actual: %s %d %f\n",
+			"A",
+			4,
+			0.0,
+			get_semitone_str(exact_note.semitone),
+			exact_note.octave,
+			exact_note.cents);
+		return 1;
+	}
 
-	printf("-----\n");
-
-	approx_note = get_approx_note(strtod(argv[1], NULL));
-	printf("%s %d\n",
-		get_semitone_str(approx_note.semitone),
-		approx_note.octave);
+	approx_note = get_approx_note(15.0);
+	if (approx_note.semitone != UNKNOWN_SEMITONE || approx_note.octave != -1 || approx_note.cents != -255.0) {
+		fprintf(
+			stderr,
+			"expected: %d %d %f, actual: %d %d %f\n",
+			UNKNOWN_SEMITONE,
+			-1,
+			-255.0,
+			approx_note.semitone,
+			approx_note.octave,
+			approx_note.cents);
+		return 1;
+	}
 
 	return 0;
 }
